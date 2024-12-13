a1702d20570d63cb5cf67d320219aef69bd4e96e
nooboftime <tamara_walling@yahoo.com>
2024-12-12 21:01:44 -0600

    deleted script js file, updated css for card flipping might take that back out, added card images and meaning

--- script.js
+++
@@ -1,2 +0,0 @@
-https://tarot-api-3hv5.onrender.com/api/v1 (dininatory meanings for our dack)
-https://github.com/msull/emilytarot (uses open ai to read the tarot)
--- tarot-api.js
+++ tarot-api.js
@@ -1,6 +1,32 @@
 // Define the Tarot API URL
 const tarotApiUrl = "https://tarot-api-3hv5.onrender.com/api/v1/cards";
 
+// Map of card names to their respective image files
+const cardImages = {
+  "The Fool": "s0-the_fool.png",
+  "The Magician": "1-the_magician.png",
+  "Wheel of Fortune": "10-wheel_of_fortune.png",
+  "Justice": "11-justice.png",
+  "The Hanged Man": "12-the_hanged_man.png",
+  "Death": "13-death.png",
+  "Temperance": "14-temperance.png",
+  "The Devil": "15-the_devil.png",
+  "The Tower": "16-the_tower.png",
+  "The Star": "17-the_star.png",
+  "The Moon": "18-the_moon.png",
+  "The Sun": "19-the_sun.png",
+  "The High Priestess": "2-the_high_priestess.png",
+  "Judgement": "20-judgement.png",
+  "The World": "21-the_world.png",
+  "The Empress": "3-the_empress.png",
+  "The Emperor": "4-the_emperor.png",
+  "The Hierophant": "5-the_hierophant.png",
+  "The Lovers": "6-the_lovers.png",
+  "The Chariot": "7-the_chariot.png",
+  "Strength": "8-strength.png",
+  "The Hermit": "9-the_hermit.png"
+};
+
 // Function to fetch card details from the Tarot API
 async function getCardDetails() {
     try {
@@ -18,9 +44,11 @@ async function getCardDetails() {
 
 // Function to display card details after a card is flipped
 function displayCardDetails(cardData, cardElement) {
+    const cardImage = cardImages[cardData.name];
     const cardText = `
         <h3>${cardData.name}</h3>
         <p>${cardData.meaning_up}</p>
+        <img src="images/${cardImage}" alt="${cardData.name}">
     `;
     cardElement.innerHTML = cardText;
 }
